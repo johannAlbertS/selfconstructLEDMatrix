6ad3a5df19933efc2c2eefe5aacfd1cbe32f3170
Albert Scherzer <johann_albert.scherzer@gmx.de>
2023-01-04 21:04:21 +0100

    Added translation to a shape on the matrix for all characters

--- software/software.ino
+++ software/software.ino
@@ -13,7 +13,7 @@ typedef struct {
   boolean arr[MATRIX_HEIGHT][MATRIX_WEIGHT];
 }slide_t;
 //String that should be displayed on the matrix
-char msg[] = "AL";
+char msg[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";//LLES";
 //Array containing the description of all letters
 boolean letters[26][MATRIX_HEIGHT][MATRIX_WEIGHT];
 
@@ -93,13 +93,111 @@ void setup() {
   pinMode(COLUMN2, OUTPUT);
   pinMode(COLUMN3, OUTPUT);
   pinMode(COLUMN4, OUTPUT);
-  
+
+  //A
   letters[0][0][0] = false; letters[0][0][1] = true; letters[0][0][2] = true; letters[0][0][3] = true;
   letters[0][1][0] = true; letters[0][1][1] = false; letters[0][1][2] = true; letters[0][1][3] = false;
-  letters[0][2][0] = false; letters[0][2][1] = true; letters[0][2][2] = true; letters[0][2][3] = true; 
+  letters[0][2][0] = false; letters[0][2][1] = true; letters[0][2][2] = true; letters[0][2][3] = true;
+  //B
+  letters[1][0][0] = true; letters[1][0][1] = true; letters[1][0][2] = true; letters[1][0][3] = true;
+  letters[1][1][0] = true; letters[1][1][1] = true; letters[1][1][2] = true; letters[1][1][3] = true;
+  letters[1][2][0] = false; letters[1][2][1] = true; letters[1][2][2] = false; letters[1][2][3] = true;
+  //C
+  letters[2][0][0] = false; letters[2][0][1] = true; letters[2][0][2] = true; letters[2][0][3] = false;
+  letters[2][1][0] = true; letters[2][1][1] = false; letters[2][1][2] = false; letters[2][1][3] = true;
+  letters[2][2][0] = true; letters[2][2][1] = false; letters[2][2][2] = false; letters[2][2][3] = true;
+  //D
+  letters[3][0][0] = true; letters[3][0][1] = true; letters[3][0][2] = true; letters[3][0][3] = true;
+  letters[3][1][0] = true; letters[3][1][1] = false; letters[3][1][2] = false; letters[3][1][3] = true;
+  letters[3][2][0] = false; letters[3][2][1] = true; letters[3][2][2] = true; letters[3][2][3] = false;
+  //E
+  letters[4][0][0] = true; letters[4][0][1] = true; letters[4][0][2] = true; letters[4][0][3] = true;
+  letters[4][1][0] = true; letters[4][1][1] = true; letters[4][1][2] = false; letters[4][1][3] = true;
+  letters[4][2][0] = true; letters[4][2][1] = true; letters[4][2][2] = false; letters[4][2][3] = true;
+  //F
+  letters[5][0][0] = true; letters[5][0][1] = true; letters[5][0][2] = true; letters[5][0][3] = true;
+  letters[5][1][0] = true; letters[5][1][1] = false; letters[5][1][2] = true; letters[5][1][3] = false;
+  letters[5][2][0] = true; letters[5][2][1] = false; letters[5][2][2] = true; letters[5][2][3] = false;
+  //G
+  letters[6][0][0] = false; letters[6][0][1] = true; letters[6][0][2] = true; letters[6][0][3] = false;
+  letters[6][1][0] = true; letters[6][1][1] = false; letters[6][1][2] = false; letters[6][1][3] = true;
+  letters[6][2][0] = true; letters[6][2][1] = false; letters[6][2][2] = true; letters[6][2][3] = true;
+  //H
+  letters[7][0][0] = true; letters[7][0][1] = true; letters[7][0][2] = true; letters[7][0][3] = true;
+  letters[7][1][0] = false; letters[7][1][1] = false; letters[7][1][2] = true; letters[7][1][3] = false;
+  letters[7][2][0] = true; letters[7][2][1] = true; letters[7][2][2] = true; letters[7][2][3] = true;
+  //I
+  letters[8][0][0] = true; letters[8][0][1] = false; letters[8][0][2] = false; letters[8][0][3] = true;
+  letters[8][1][0] = true; letters[8][1][1] = true; letters[8][1][2] = true; letters[8][1][3] = true;
+  letters[8][2][0] = true; letters[8][2][1] = false; letters[8][2][2] = false; letters[8][2][3] = true;
+  //J
+  letters[9][0][0] = true; letters[9][0][1] = false; letters[9][0][2] = true; letters[9][0][3] = false;
+  letters[9][1][0] = true; letters[9][1][1] = false; letters[9][1][2] = false; letters[9][1][3] = true;
+  letters[9][2][0] = true; letters[9][2][1] = true; letters[9][2][2] = true; letters[9][2][3] = false;
+  //K
+  letters[10][0][0] = true; letters[10][0][1] = true; letters[10][0][2] = true; letters[10][0][3] = true;
+  letters[10][1][0] = false; letters[10][1][1] = false; letters[10][1][2] = true; letters[10][1][3] = false;
+  letters[10][2][0] = true; letters[10][2][1] = false; letters[10][2][2] = false; letters[10][2][3] = true;
+  //L
   letters[11][0][0] = true; letters[11][0][1] = true; letters[11][0][2] = true; letters[11][0][3] = true;
   letters[11][1][0] = false; letters[11][1][1] = false; letters[11][1][2] = false; letters[11][1][3] = true;
   letters[11][2][0] = false; letters[11][2][1] = false; letters[11][2][2] = false; letters[11][2][3] = true;
+  //M
+  letters[12][0][0] = true; letters[12][0][1] = true; letters[12][0][2] = true; letters[12][0][3] = true;
+  letters[12][1][0] = false; letters[12][1][1] = true; letters[12][1][2] = false; letters[12][1][3] = false;
+  letters[12][2][0] = true; letters[12][2][1] = true; letters[12][2][2] = true; letters[12][2][3] = true;
+  //N
+  letters[13][0][0] = true; letters[13][0][1] = true; letters[13][0][2] = true; letters[13][0][3] = true;
+  letters[13][1][0] = false; letters[13][1][1] = true; letters[13][1][2] = true; letters[13][1][3] = false;
+  letters[13][2][0] = true; letters[13][2][1] = true; letters[13][2][2] = true; letters[13][2][3] = true;
+  //O
+  letters[14][0][0] = false; letters[14][0][1] = true; letters[14][0][2] = true; letters[14][0][3] = false;
+  letters[14][1][0] = true; letters[14][1][1] = false; letters[14][1][2] = false; letters[14][1][3] = true;
+  letters[14][2][0] = false; letters[14][2][1] = true; letters[14][2][2] = true; letters[14][2][3] = false;
+  //P
+  letters[15][0][0] = true; letters[15][0][1] = true; letters[15][0][2] = true; letters[15][0][3] = true;
+  letters[15][1][0] = true; letters[15][1][1] = false; letters[15][1][2] = true; letters[15][1][3] = false;
+  letters[15][2][0] = false; letters[15][2][1] = true; letters[15][2][2] = false; letters[15][2][3] = false;
+  //Q
+  letters[16][0][0] = false; letters[16][0][1] = true; letters[16][0][2] = true; letters[16][0][3] = false;
+  letters[16][1][0] = true; letters[16][1][1] = false; letters[16][1][2] = true; letters[16][1][3] = true;
+  letters[16][2][0] = false; letters[16][2][1] = true; letters[16][2][2] = false; letters[16][2][3] = true;
+  //R
+  letters[17][0][0] = true; letters[17][0][1] = true; letters[17][0][2] = true; letters[17][0][3] = true;
+  letters[17][1][0] = true; letters[17][1][1] = false; letters[17][1][2] = true; letters[17][1][3] = false;
+  letters[17][2][0] = false; letters[17][2][1] = true; letters[17][2][2] = false; letters[17][2][3] = true;
+  //S
+  letters[18][0][0] = false; letters[18][0][1] = true; letters[18][0][2] = false; letters[18][0][3] = true;
+  letters[18][1][0] = true; letters[18][1][1] = false; letters[18][1][2] = true; letters[18][1][3] = true;
+  letters[18][2][0] = true; letters[18][2][1] = false; letters[18][2][2] = true; letters[18][2][3] = false;
+  //T
+  letters[19][0][0] = true; letters[19][0][1] = false; letters[19][0][2] = false; letters[19][0][3] = false;
+  letters[19][1][0] = true; letters[19][1][1] = true; letters[19][1][2] = true; letters[19][1][3] = true;
+  letters[19][2][0] = true; letters[19][2][1] = false; letters[19][2][2] = false; letters[19][2][3] = false;
+  //U
+  letters[20][0][0] = true; letters[20][0][1] = true; letters[20][0][2] = true; letters[20][0][3] = false;
+  letters[20][1][0] = false; letters[20][1][1] = false; letters[20][1][2] = false; letters[20][1][3] = true;
+  letters[20][2][0] = true; letters[20][2][1] = true; letters[20][2][2] = true; letters[20][2][3] = true;
+  //V
+  letters[21][0][0] = true; letters[21][0][1] = true; letters[21][0][2] = true; letters[21][0][3] = false;
+  letters[21][1][0] = false; letters[21][1][1] = false; letters[21][1][2] = false; letters[21][1][3] = true;
+  letters[21][2][0] = true; letters[21][2][1] = true; letters[21][2][2] = true; letters[21][2][3] = false;
+  //W
+  letters[22][0][0] = true; letters[22][0][1] = true; letters[22][0][2] = true; letters[22][0][3] = true;
+  letters[22][1][0] = true; letters[22][1][1] = true; letters[22][1][2] = true; letters[22][1][3] = false;
+  letters[22][2][0] = true; letters[22][2][1] = true; letters[22][2][2] = true; letters[22][2][3] = true;
+  //X
+  letters[23][0][0] = true; letters[23][0][1] = false; letters[23][0][2] = false; letters[23][0][3] = true;
+  letters[23][1][0] = false; letters[23][1][1] = true; letters[23][1][2] = true; letters[23][1][3] = false;
+  letters[23][2][0] = true; letters[23][2][1] = false; letters[23][2][2] = false; letters[23][2][3] = true;
+  //Y
+  letters[24][0][0] = true; letters[24][0][1] = true; letters[24][0][2] = false; letters[24][0][3] = false;
+  letters[24][1][0] = false; letters[24][1][1] = false; letters[24][1][2] = true; letters[24][1][3] = true;
+  letters[24][2][0] = true; letters[24][2][1] = true; letters[24][2][2] = false; letters[24][2][3] = false;
+  //Z
+  letters[25][0][0] = true; letters[25][0][1] = false; letters[25][0][2] = true; letters[25][0][3] = true;
+  letters[25][1][0] = true; letters[25][1][1] = true; letters[25][1][2] = false; letters[25][1][3] = true;
+  letters[25][2][0] = true; letters[25][2][1] = false; letters[25][2][2] = false; letters[25][2][3] = true;
 }
 
 void loop() {
